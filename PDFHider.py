56773ba0a2bf3ebe85ff1708ef8d05df93c829e3
Nicholas Tateo <tateo.nick@gmail.com>
2019-12-03 21:08:42 -0500

    Add files via upload

---
+++ PDFHider.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+#Pedro Sicilia
+#Nicholas Tateo
+#CIS4362
+#PDFHider: PDF angecryption CLI tool 
+
+import sys
+
+def myfunction():
+	print "do stuff"
+	
+#define functions here
+
+
+#---Begin main script---
+
+
+#do stuff here
+myfunction()
+
+
+#----------End----------
