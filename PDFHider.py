7688e67fa5f730fa7ef853a87b0aafa2beaa0632
Nicholas Tateo <tateo.nick@gmail.com>
2019-12-04 04:36:05 -0500

    arg checking, AES module accessible

--- PDFHider.py
+++ PDFHider.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 #Pedro Sicilia
 #Nicholas Tateo
@@ -6,18 +6,35 @@
 #PDFHider: PDF angecryption CLI tool 
 
 import sys
+from Crypto.Cipher import AES
 
-def myfunction():
-	print "do stuff"
+#first cipher block: "%PDF-\0obj\nstream"
+test_key = "ABCDEFGHIJKLMNOP"
+
+def checkArgs(v):
+	if(len(sys.argv) == 4):
+		infile1, infile2, outfile = sys.argv[1:4]
+		print("\nArguments:\n", infile1, infile2, outfile, "\nEnd")
+		return True
+		
+	elif(len(sys.argv) > 4):
+		print("Error: excess arguments\nUsage: ./PDFHider <input1.pdf> <input2.pdf> <output.pdf>")
+	else:
+		print("Error: missing arguments\nUsage: ./PDFHider <input1.pdf> <input2.pdf> <output.pdf>")
+
+	return False
 	
-#define functions here
+#def checkFiles():
+	#with open(fname1, "rb") as f1
+		#assert f1.startswith
+
+#def pad():
+	
+
+#----------Begin main script----------
+
+if(checkArgs(sys.argv)):
+	print(AES.block_size, AES.MODE_CBC)
 
 
-#---Begin main script---
-
-
-#do stuff here
-myfunction()
-
-
-#----------End----------
+#-----------------End-----------------
